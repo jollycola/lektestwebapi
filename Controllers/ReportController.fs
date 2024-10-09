d406ccd72931e4a4f7846b7c8cc63cc3fa08b316
jollycola <kleiven76@gmail.com>
2024-10-09 08:43:30 +0200

    initial commit

---
+++ Controllers/ReportController.fs
@@ -0,0 +1,19 @@
+﻿namespace ReportController
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open Report
+
+[<ApiController>]
+[<Route("[controller]")>]
+type ReportController (logger : ILogger<ReportController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        { Date = DateTime.Now
+          ReportText = "Test"}
---
+++ Report.fs
@@ -0,0 +1,7 @@
+namespace Report
+
+open System
+
+type Report =
+    { Date: DateTime
+      ReportText: string }
